559efd3762ef74a891a2cc48c340656613a574ed
Adrian Haasler García <dev@adrianhaasler.com>
2015-04-12 11:44:32 +0200

    Load link contents with AJAX and update the rendered page with it
    This is something I wanted to do at the end, or at least not right now. But I
    came across https://github.com/joelhans/Jekyll-AJAX by @joelhans and it was easy
    to adapt it to colos. That project doesn't have a LICENSE file
    (joelhans/Jekyll-AJAX#2), but since that project has another contributor and
    seeing the contents of the README I suppose it's absent by mistake. So I used
    the code as it were MIT compatible.
    
    > From the README: The best way to get functionality out-of-the-box is to clone
    > this repo out of Github and use it as a starting point for your own creation.
    > You can change the formatting, the types of layouts — whatever you want.

---
+++ js/navigation.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function() {
+	var siteUrl = 'http://' + (document.location.hostname || document.location.host);
+
+	// Catch all internally-focused links and push a new state.
+	$(document).delegate('a[href^="/"],a[href^="' + siteUrl + '"]', "click", function(e) {
+		e.preventDefault();
+		History.pushState({}, "", this.pathname);
+	});
+
+	History.Adapter.bind(window, 'statechange', function(){
+		var state = History.getState();
+		// Get link content
+		$.get(state.url, function(data) {
+			// Update html language tag
+			$('html').attr({'lang':data.match(/<html lang=\"(.*?)\">/)[1]});
+			// Update html title
+			document.title = data.match(/<title>(.*?)<\/title>/)[1];
+			// Update canonical link
+			$('link[rel=canonical]').attr({'href':data.match(/<link rel=\"canonical\" href=\"(.*?)\">/)[1]});
+			// Update body class (colors)
+			document.body.className = data.match(/<body class=\"(.*?)\"/)[1];
+			// Update content
+			$('.content').html($(data).find('.content'));
+			// Update header title
+			$('.header div').html($(data).find('.header div'));
+			// Update language links
+			$('.header .menu').html($(data).find('.header .menu'));
+			// Update drawer links
+			$('[drawer] core-menu').html($(data).find('[drawer] core-menu'));
+		});
+	});
+});
